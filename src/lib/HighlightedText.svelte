6d0564a3ff649dce4da1e4db53a5302ce2cfb40a
darrelhong <darrel.hong@gmail.com>
2021-05-09 09:27:47 +0800

    initial commit with svkit

---
+++ src/lib/HighlightedText.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let color;
+	export let colorDark = color;
+	export let href;
+</script>
+
+<a {href} class="highlight" style="--highlight-color: {color};  --highlight-color-dark: {colorDark}">
+	<slot />
+</a>
+
+<style>
+	.highlight {
+		text-decoration: none;
+		color: rgb(var(--highlight-color));
+		box-shadow: 0 10px 0 0 rgb(var(--highlight-color), 0),
+			inset 0 -0.125em rgb(var(--highlight-color), 0.2);
+		transition-property: box-shadow, color;
+		transition-duration: 0.2s;
+		border-radius: 3px;
+	}
+
+	:global(:root[data-theme='dark'] .highlight) {
+		color: rgb(var(--highlight-color-dark));
+		box-shadow: 0 10px 0 0 rgb(var(--highlight-color-dark), 0),
+			inset 0 -0.125em rgb(var(--highlight-color-dark), 0.2);
+	}
+
+	.highlight:hover {
+		color: rgb(var(--highlight-color));
+		box-shadow: 0 1px 0 4px rgb(var(--highlight-color), 0.1),
+			inset 0 -1.375em 0 rgba(var(--highlight-color), 0.1);
+	}
+
+	:global(:root[data-theme='dark'] .highlight:hover) {
+		color: rgb(var(--highlight-color-dark));
+		box-shadow: 0 1px 0 4px rgb(var(--highlight-color-dark), 0.1),
+			inset 0 -1.375em 0 rgba(var(--highlight-color-dark), 0.1);
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import DarkModeButton from '$lib/DarkModeButton.svelte';
+	import HighlightedText from '$lib/HighlightedText.svelte';
+</script>
+
+<svelte:head>
+	<title>@darrelhong</title>
+</svelte:head>
+
+<DarkModeButton />
+<h1>Hi, I'm Darrel Hong</h1>
+<p>
+	I'm a <HighlightedText color="16, 185, 129" colorDark="110, 231, 183"
+		>software engineer</HighlightedText
+	>, <HighlightedText color="59, 130, 246" colorDark="96, 165, 250">web developer</HighlightedText> and
+	<HighlightedText color="244, 114, 182">photography enthusiast</HighlightedText>.
+</p>
+<p class="find-me">
+	Find me on <HighlightedText
+		color="34, 89, 156"
+		colorDark="82, 134, 196"
+		href="https://linkedin.com">LinkedIn</HighlightedText
+	> and
+	<HighlightedText color="24, 27, 31" colorDark="201, 209, 217" href="https://github.com"
+		>Github</HighlightedText
+	>.
+</p>
+
+<style>
+	:global(:root) {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu,
+			Cantarell, 'Helvetica Neue', sans-serif;
+		background-color: white;
+		color: #1f2937;
+		transition: background-color 0.3s;
+	}
+
+	:global(button) {
+		cursor: pointer;
+	}
+
+	:global(:root[data-theme='dark']) {
+		background-color: #0e1421;
+		color: white;
+	}
+</style>
